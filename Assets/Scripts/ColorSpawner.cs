cfeff969f0cd454a13df90319649a77229e0da70
AQUIRISX\gabriel.junior <gabriel.junior@aquiris.com.br>
2019-05-29 17:06:47 -0300

    Unity2D Assets - 2018.2.7f1

---
+++ Assets/Scripts/ColorSpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorSpawner : MonoBehaviour {
+
+	public GameObject[] CubeColor;
+
+
+	//Leave public for debug 12092003
+	public bool _spawn = true;
+
+	void Start () {
+
+	}
+
+	void Update () {
+		if (_spawn) {
+			for (int x = 0; x < CubeColor.Length; x++) {
+				Instantiate (CubeColor[x], new Vector3 (Random.Range (-15.94f, 15.94f), Random.Range (-7.7f, 7.7f), 1), Quaternion.identity);
+			}
+			_spawn = false;
+		}
+	}
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+	private Rigidbody2D _rig;
+
+	public float Speed;
+
+	// Use this for initialization
+	void Start () {
+		_rig = GetComponent<Rigidbody2D> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		float moveX = Input.GetAxis ("Horizontal");
+		float moveY = Input.GetAxis ("Vertical");
+
+		Vector2 movement = new Vector2 (moveX, moveY);
+		_rig.AddForce (movement * Speed);
+	}
+}
